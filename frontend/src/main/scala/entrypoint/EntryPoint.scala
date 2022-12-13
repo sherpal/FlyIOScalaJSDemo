9488174e29a3656ac865b11fc1925ca6a9ee9699
Antoine Doeraene <antoine.doeraene@gmail.com>
2022-12-13 21:17:29 +0100

    Some cleaning

--- frontend/src/main/scala/entrypoint/EntryPoint.scala
+++ frontend/src/main/scala/entrypoint/EntryPoint.scala
@@ -11,52 +11,48 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 import scala.scalajs.js
 
-object EntryPoint {
-
-  def main(args: Array[String]): Unit = {
-    def request(dataToSend: SomeSharedData) = new dom.RequestInit {
-      method = dom.HttpMethod.POST
-      body = dataToSend.asJson.noSpaces
-      headers = js.Array(
-        js.Array("Content-Type", "application/json"),
-        js.Array("Accept", "text/plain")
-      )
-    }
-
-    val clickBus: EventBus[Unit] = new EventBus
-    val textToSendVar            = Var("")
-
-    render(
-      dom.document.getElementById("root"),
-      div(
-        Title.h1("Demo fly.io"),
-        p(
-          child.text <-- clickBus.events
-            .sample(textToSendVar.signal)
-            .flatMap(text =>
-              EventStream.fromFuture(
-                dom
-                  .fetch("/api/do-thing", request(SomeSharedData(text, 2)))
-                  .toFuture
-                  .flatMap(_.text().toFuture),
-                emitFutureIfCompleted = true
-              )
-            )
-        ),
-        div(
-          display    := "flex",
-          alignItems := "center",
-          Label("Enter some text: "),
-          Input(
-            _.tpe := InputType.Text,
-            _.events.onChange.mapToValue --> textToSendVar.writer,
-            marginRight := "1em",
-            marginLeft  := "1em"
-          ),
-          Button("Click me", _.events.onClick.mapTo(()) --> clickBus.writer)
-        )
-      )
+@main def run(): Unit = {
+  def request(dataToSend: SomeSharedData) = new dom.RequestInit {
+    method = dom.HttpMethod.POST
+    body = dataToSend.asJson.noSpaces
+    headers = js.Array(
+      js.Array("Content-Type", "application/json"),
+      js.Array("Accept", "text/plain")
     )
   }
 
+  val clickBus: EventBus[Unit] = new EventBus
+  val textToSendVar            = Var("")
+
+  render(
+    dom.document.getElementById("root"),
+    div(
+      Title.h1("Demo fly.io"),
+      p(
+        child.text <-- clickBus.events
+          .sample(textToSendVar.signal)
+          .flatMap(text =>
+            EventStream.fromFuture(
+              dom
+                .fetch("/api/do-thing", request(SomeSharedData(text, 2)))
+                .toFuture
+                .flatMap(_.text().toFuture),
+              emitFutureIfCompleted = true
+            )
+          )
+      ),
+      div(
+        display    := "flex",
+        alignItems := "center",
+        Label("Enter some text: "),
+        Input(
+          _.tpe := InputType.Text,
+          _.events.onChange.mapToValue --> textToSendVar.writer,
+          marginRight := "1em",
+          marginLeft  := "1em"
+        ),
+        Button("Click me", _.events.onClick.mapTo(()) --> clickBus.writer)
+      )
+    )
+  )
 }
--- server/src/main/scala/server/Server.scala
+++ server/src/main/scala/server/Server.scala
@@ -9,39 +9,35 @@ import business.SomeSharedData
 import path.Path
 import scala.scalajs.LinkingInfo
 
-object Server {
+@main def launchServer(): Unit = {
+  def devProdValue[A](forDev: => A, forProd: => A): A =
+    if LinkingInfo.developmentMode then forDev else forProd
 
-  private def devProdValue[A](forDev: => A, forProd: => A): A = if LinkingInfo.developmentMode then forDev else forProd
+  val port = devProdValue(9000, 8080)
+  val host = devProdValue("localhost", "0.0.0.0")
 
-  def main(args: Array[String]): Unit = {
-    println("hello from publish")
-    val port = devProdValue(9000, 8080)
-    val host = devProdValue("localhost", "0.0.0.0")
-
-    val app = Express()
-    app.get("/", (_, res) => res.sendFile(Path.pwd :/ "static/static/index.html"))
-    app.use("/api/do-thing", Express.json())
-    app.post(
-      "/api/do-thing",
-      { (req, res) =>
-        decodeJs[SomeSharedData](req.body) match {
-          case Left(err)    => res.status(400).send(err.getMessage)
-          case Right(value) => res.send(s"I did something very cool with your $value")
-        }
+  val app = Express()
+  app.get("/", (_, res) => res.sendFile(Path.pwd :/ "static/static/index.html"))
+  app.use("/api/do-thing", Express.json())
+  app.post(
+    "/api/do-thing",
+    { (req, res) =>
+      decodeJs[SomeSharedData](req.body) match {
+        case Left(err)    => res.status(400).send(err.getMessage)
+        case Right(value) => res.send(s"I did something very cool with your $value")
       }
-    )
-    app.use(Express.static("static"))
+    }
+  )
+  app.use(Express.static("static"))
 
-    app.useInProduction(Compression())
+  app.useInProduction(Compression())
 
-    app.listen(
-      port,
-      host,
-      () => {
-        dom.console.log(s"Started listening to $port")
-      }
-    )
-
-  }
+  app.listen(
+    port,
+    host,
+    () => {
+      dom.console.log(s"Started listening to $port")
+    }
+  )
 
 }
